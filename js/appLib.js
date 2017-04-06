aea86c5f5ddde0d2b25f34bb99e707da7be50b80
magneto.1990@gmail.com <nexus@123>
2017-04-06 14:00:58 +0530

    Commit Code : Buyer_Supplier_Auction_V4.8 Commit For : Easy PR Multitenant App Back Button

--- js/appLib.js
+++ js/appLib.js
@@ -106,6 +106,7 @@ function goBackEvent() {
 				//navigator.notification.confirm("Are you sure want to exit from App?", onConfirmExit, "Confirmation", "Yes,No");
 			}else{
 					var pg=appPageHistory[len-1];
+					var currentPage=appPageHistory[len];
 					alert("page "+pg);
 					if(pg=="app/pages/addPurchaseReq.html"){ 
 							 backBtn = true;
@@ -125,7 +126,7 @@ function goBackEvent() {
 						 headerBackBtn=defaultPagePath+'expenzingImageWithSyncPage.html';	
 						 j('#mainHeader').load(headerBackBtn);
 						 j('#mainContainer').load(pageRef);	
-					}else if(pg=="app/pages/prInvoice.html"){
+					}else if(currentPage != "app/pages/prInvoice.html" && pg=="app/pages/prInvoice.html"){
 						if(confirmToGoBack()==false){
 							if(confirm("All the filled in details will be deleted. Do you want to Proceed app lib?")==false){
 								return false;
